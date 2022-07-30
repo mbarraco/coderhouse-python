a85a85db748e25861740caa519dad8e55a8f2b21
mariano <marianobarraco@gmail.com>
2022-07-30 10:15:58 -0300

    clase 01

--- clase_01.py
+++ clase_01.py
@@ -1,2 +1,18 @@
+# definir variables
+mi_edad = 27
+mi_nombre = "Diego"
 
-# definir una variable
+
+# imprimir variables
+print(mi_edad)
+print(mi_nombre)
+
+
+# preguntar el tipo de dato en una variable
+type(mi_edad)
+type(mi_nombre)
+
+
+# preguntar la longitud de unn string
+len(mi_nombre)
+
