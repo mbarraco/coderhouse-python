cffd3f80b05739240158c639121059a2a87edf97
mariano <marianobarraco@gmail.com>
2022-08-17 21:14:22 -0300

    clase 07 bis

--- clase_07.py
+++ clase_07.py
@@ -10,3 +10,27 @@ agenda = agregar_contacto_a_agenda("facundo", "facundo@coder.com", agenda)
 agenda = agregar_contacto_a_agenda("mariano", "este mail es incorrecto", agenda)
 print(agenda)
 
+
+
+# CLASE 07
+mi_diccionario = {'mariano': 37, 'juan': 38, 'alberto': 80}
+
+for clave, valor in mi_diccionario.items():
+    print("la clave es: " + str(clave))
+    print("el valor es: " + str(valor))
+    print("*" * 90)
+
+
+
+print("." * 90)
+print("." * 90)
+print("." * 90)
+
+# EJERCICIO SUPER DIFICIL, caso de estudio: imprimir elementos ordenados del diccionario
+claves =  list(mi_diccionario.keys())
+claves.sort()
+
+for clave in claves:
+    print("la clave es: " + str(clave))
+    print("el valor es: " + str(mi_diccionario[clave]))
+    print("*" * 90)
